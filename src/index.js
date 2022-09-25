81e641fe8e115fe6db78c644e83c560b6aee54b0
Robert Stebbins <48532176+robertstebbins@users.noreply.github.com>
2022-09-25 02:47:53 -0500

    prac

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-// import your function
-import myName from './myName';
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
-  // use your function!
-  element.textContent = myName('Cody');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+ console.log(Data);
+ console.log(Notes);
+
   return element;
 }
 
